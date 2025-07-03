2e79c1d49d3525e4b58fb1ad5cd32d55e9391741
Shahith R <87928073+Shahithraj@users.noreply.github.com>
2025-07-03 19:25:30 +0530

    Update classes.ts

--- Advanced/classes.ts
+++ Advanced/classes.ts
@@ -1,25 +1,68 @@
-// class User {
-//     public name : string;
-//     age: number;
-//     private readonly city: string;
-//     constructor(name:string, age:number) {
-//         this.name = name;
-//         this.age = age;
-//     }
-// }
-
-// short and optimized way
-
 class User {
-    constructor(
-        public name:string, 
-        private age:number,
-        public userId:string
-    ) {
+
+    public name : string;
+    age: number;
+    private readonly city: string;
+
+    constructor(name:string, age:number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private deleteUser() {
+        this.name = "";
+        this.age = 0;
+    }
+
+    // public wrapper
+    public performDelete() {
+        this.deleteUser();
+    }
+
+    get getName(): string  {
+        return this.name;
+    }
+
+    set setName(name: string) {
+        this.name = name;
+    }
+
+    set setAge(age:number) {
+        if(age < 18) {
+            this.age = 18;
+            throw new Error("Not legal age");
+        }
+        this.age = age;
+
+    }
+
+    get getCity():string {
+        return this.city;
     }
 }
 
-const person = new User("shahith",23,"");
+// short and optimized way
+
+// class User {
+//     constructor(
+//         public name:string, 
+//         private age:number,
+//         public userId:string
+//     ) {
+//     }
+// }
+
+
+
+const person = new User("shahith",23);
+
+console.log(person.getName);    // Outputs: "shahith"
+person.setName = "abith";
+person.setAge = 10;
+console.log(person.getName);    // Outputs: "abith"
+person.performDelete();
+console.log(person.getName);    // Outputs: ""
+
 
 // public will be default
 
