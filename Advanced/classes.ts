438a76f2ffb83b8ee572270fe6e1072d81923625
Shahith R <shahithr99@gmail.com>
2025-07-06 11:18:25 +0530

    classes added

--- Advanced/classes.ts
+++ Advanced/classes.ts
@@ -1,71 +1,83 @@
 class User {
+  public name: string;
+  age: number;
+  private readonly city: string = 'Chennai';
+  protected email: string;
 
-    public name : string;
-    age: number;
-    private readonly city: string;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
-    constructor(name:string, age:number) {
-        this.name = name;
-        this.age = age;
+  private deleteUser() {
+    this.name = '';
+    this.age = 0;
+  }
+
+  // public wrapper
+  public performDelete() {
+    this.deleteUser();
+  }
+
+  get getName(): string {
+    return this.name;
+  }
+
+  set setName(name: string) {
+    this.name = name;
+  }
+
+  set setAge(age: number) {
+    if (age < 18) {
+      this.age = 18;
+      throw new Error('Not legal age');
     }
+    this.age = age;
+  }
 
-    private deleteUser() {
-        this.name = "";
-        this.age = 0;
-    }
-
-    // public wrapper
-    public performDelete() {
-        this.deleteUser();
-    }
-
-    get getName(): string  {
-        return this.name;
-    }
-
-    set setName(name: string) {
-        this.name = name;
-    }
-
-    set setAge(age:number) {
-        if(age < 18) {
-            this.age = 18;
-            throw new Error("Not legal age");
-        }
-        this.age = age;
-
-    }
-
-    get getCity():string {
-        return this.city;
-    }
+  get getCity(): string {
+    return this.city;
+  }
 }
 
 // short and optimized way
 
 // class User {
 //     constructor(
-//         public name:string, 
+//         public name:string,
 //         private age:number,
 //         public userId:string
 //     ) {
 //     }
 // }
 
+// private variable in the User class cannot be access in the SubUser but public and protected can.
 
+// Protected can be access only in same class and inheritance class and not outside.
 
-const person = new User("shahith",23);
+class SubUser extends User {
+  isFamily: boolean;
+  changeEmail() {
+    this.email = 'sha@gmail.com';
+  }
+}
 
-console.log(person.getName);    // Outputs: "shahith"
-person.setName = "abith";
+const person = new User('shahith', 23);
+
+console.log(person.getName); // Outputs: "shahith"
+person.setName = 'abith';
 person.setAge = 10;
-console.log(person.getName);    // Outputs: "abith"
+console.log(person.getName); // Outputs: "abith"
 person.performDelete();
-console.log(person.getName);    // Outputs: ""
+console.log(person.getName); // Outputs: ""
 
+const subPerson = new SubUser('senthil', 45);
+
+// public, private and protected are access modifiers that limit the access
 
 // public will be default
 
+// private can access only within the class
 
 
 export {};
--- Advanced/interface.ts
+++ Advanced/interface.ts
@@ -1,62 +1,97 @@
-"use strict";
+'use strict';
 interface User {
-    readonly id :number;
-    name:string;
-    email?:string;
-    //startTrial: () => string;
-    startTrial() : string;
-    getCoupon(name:string, val?:number): number
+  readonly id: number;
+  name: string;
+  email?: string;
+  //startTrial: () => string;
+  startTrial(): string;
+  getCoupon(name: string, val?: number): number;
 }
 
 interface User {
-    googleId:string;
+  googleId: string;
 }
 
 interface Admin extends User {
-    role:string;
+  role: string;
 }
 
 const myUser: User = {
-    id:1,
-    name:"shahith",
-    startTrial () {
-        return ""
-    },
-    getCoupon:(name: string, val?:number) => {
-        return 100;
-    },
-    googleId:"sha@gmail.com"
-}
+  id: 1,
+  name: 'shahith',
+  startTrial() {
+    return '';
+  },
+  getCoupon: (name: string, val?: number) => {
+    return 100;
+  },
+  googleId: 'sha@gmail.com',
+};
 
-const admin : Admin = {
-     id:1,
-    name:"shahith",
-    startTrial () {
-        return ""
-    },
-    getCoupon:(name: string, val?:number) => {
-        return 100;
-    },
-    googleId:"sha@gmail.com",
-    role:"Admin",
-}
+const admin: Admin = {
+  id: 1,
+  name: 'shahith',
+  startTrial() {
+    return '';
+  },
+  getCoupon: (name: string, val?: number) => {
+    return 100;
+  },
+  googleId: 'sha@gmail.com',
+  role: 'Admin',
+};
 
 console.log(myUser.getCoupon(myUser.name));
 
+// Why Interface is important
 
-interface A { a: number }
-interface B extends A { b: number }
+interface TakePhoto {
+  cameroMode: string;
+  filter: string;
+  edit: boolean;
+  createVideo(): void;
+}
+
+class Instagram implements TakePhoto {
+  constructor(
+    public cameroMode: string,
+    public filter: string,
+    public edit: boolean
+  ) {}
+
+  createVideo(): void {
+    console.log('Creating Instagram video...');
+  }
+}
+
+// You cannot define a method like createVideo(): void directly in the constructor like a property.
+
+// In TypeScript, methods should be declared inside the class body, not in the constructor.
+
+class YouTube implements TakePhoto {
+  constructor(
+    public cameroMode: string,
+    public filter: string,
+    public edit: boolean,
+    public short: string
+  ) {}
+
+  createVideo(): void {
+    console.log('Creating YouTube video...');
+  }
+}
+
+// interface A { a: number }
+// interface B extends A { b: number }
 
 // type: flexible, composable
-type A = { a: number }
-type B = A & { b: number }
+// type A = { a: number }
+// type B = A & { b: number }
 
+// interface vs type
 
-// interface vs type 
+// 1. Cannot duplicate type but interface can.
+// 2. Interface can extends but type can using &.
+// 3. type can have primitive type
 
-1. Cannot duplicate type but interface can.
-2. Interface can extends but type can using &.
-3. type can have primitive type
-
-
-export {}
+export {};
